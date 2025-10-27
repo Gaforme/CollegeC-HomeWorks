62d5d74b874eb603cf193758df6bd2405fe76dc8
Gaforme <smoy_boy@mail.ru>
2025-10-27 09:53:03 +0300

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа вычисления факториала");
+ 
+        Console.Write("Введите число для вычисления факториала: ");
+        string input = Console.ReadLine();
+ 
+        if (!int.TryParse(input, out int number) || number < 0)
+        {
+            Console.WriteLine("Ошибка: введите положительное целое число");
+            return;
+        }
+        
+        long factorial = CalculateFactorial(number);
+ 
+        Console.WriteLine($"Факториал числа {number} равен {factorial}");
+  
+        Console.ReadKey();
+    }
+ 
+    static long CalculateFactorial(int n)
+    {
+        long result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
