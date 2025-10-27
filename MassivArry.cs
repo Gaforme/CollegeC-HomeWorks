1c414050bc23761737b96c4aedf88b532afec308
Gaforme <smoy_boy@mail.ru>
2025-10-27 10:32:39 +0300

    Add files via upload

---
+++ MassivArry.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4 };
+        foreach (int value in array)
+        {
+            Console.Write(value);
+        }
+        Console.WriteLine();
+
+
+        Array.Reverse(array);
+        foreach (int value in array)
+        {
+            Console.Write(value);
+        }
+        Console.WriteLine();
+        }
+    }
